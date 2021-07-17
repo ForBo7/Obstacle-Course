c63c47a4ba03b8f6365fbb7dcccb31b485c0edeb
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-17 13:38:10 +0500

    Refactored the player movement and added an introduction in the Unity console.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,13 +12,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintIntroduction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Translate the object.
+        MovePlayer();
+    }
+
+    void PrintIntroduction()
+    {
+        /* Introduce the player. */
+        Debug.Log("Welcome to Obstacle Course!");
+        Debug.Log("Avoid obstacles to win the game.");
+        Debug.Log("Use WASD or the arrow keys to control Cap Man.");
+    }
+
+    void MovePlayer()
+    {
+        /* Translate the object. */
         xIncrement = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         zIncrement = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
         transform.Translate(xIncrement, 0f, zIncrement);
